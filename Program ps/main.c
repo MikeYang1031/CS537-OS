6193310a116f5d9ec1f3a5843150dd9c62da3afe
Hyukjoon Yang <hyang348@wisc.edu>
2020-09-18 23:50:10 -0500

    Update main.c

--- Program ps/main.c
+++ Program ps/main.c
@@ -33,8 +33,7 @@ int main(int argc, char *argv[])
   parsing part
   */
   flags * flag = parseArgument(argc, argv);
-  printf("pidflag: %d, stateflag: %d, stimeflag: %d, vmemflag: %d, utimeflag: %d, cmdflag: %d\n", flag->pidflag, flag->stateflag, flag->stimeflag,flag->vmemflag, flag->utimeflag, flag->cmdflag);
-    
+
   /*
   reading part
   */
@@ -44,14 +43,15 @@ int main(int argc, char *argv[])
       readPidandPrint(flag, flag->pid);
   }
   else {
-       char ** pidList = scanDirectory(flag);     
+      char ** pidList = scanDirectory(flag);     
       
       for(int i = 0; i < MAX_NUM_PID; i++)
       {
       
           if(pidList[i][0] != 0)
           {
-              printf("%s\n", pidList[i]);
+              //printf("%s\n", pidList[i]);
+              readPidandPrint(flag, pidList[i]);
           }
       }
       
@@ -63,7 +63,10 @@ int main(int argc, char *argv[])
       }
       free(pidList);
   }
+  
     
+  
+  
   free(flag);
   return 0;
 }
