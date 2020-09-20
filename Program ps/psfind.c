54784630ba45fb8486942f898c9725d0d61d36fe
Hyukjoon Yang <hyang348@wisc.edu>
2020-09-20 00:45:47 -0500

    Update psfind.c

--- Program ps/psfind.c
+++ Program ps/psfind.c
@@ -9,7 +9,7 @@
 *
 *
 *
-* AUTHOR :    Jiwon Song  HyukJun Yang   
+* AUTHOR :    Jiwon Song  HyukJoon Yang   
 *
 *
 *
@@ -17,178 +17,192 @@
 
 #include "psfind.h"
 
+/*
+ * Function: parseArgument
+ * --------------------
+ * This function reads user input from command and sets up the flags for each.
+ * If there is no pid read from the input, it returns flag struct with the pid
+ * flag is false. Otherwise, all flags will be set up based on the user input
+ *
+ *  argc : is the length of the user input from the command
+ *  argv : is a pointer to a character array that stores the user input from
+ *         the command
+ */
 flags *parseArgument(int argc, char *argv[])
 {
   // variables for option and PID number
-  int opt = 0, i = 0;
-  flags *f = malloc(sizeof(flags));
-  if (f == NULL)
-  {
-    fprintf(stderr, "unavailable to allocate memory for flags\n");
-    exit(EXIT_FAILURE);
-  }
-
-  /*
-  declaring flags
-  */
-  int pidflag = 0, stateflag = 0, stimeflag = 0, vmemflag = 0; // flags with default value False
-  int utimeflag = 1, cmdflag = 1;                              // flags with default value True
-
-    int test1 = 0;
-    
-  //  If there is an error in the command line, print and error and exit.  Don't try to go forward on questionable information.
-  while ((opt = getopt(argc, argv, "p:S::s::U::v::c::")) != -1)
-  {
-    // turn off error message
-    opterr = 0;
-
-      switch (opt)
-    {
-
-    case 'p':
-      if (optarg != NULL)
-      {
-        int valid = 1, size = strlen(optarg);
-
-        for (i = 0; i < size; i++)
-        {
-          if (!isdigit(optarg[i]))
-          {
-            valid = 0;
-          }
-        }
-
-        if (valid)
-        {
-          f->pid = optarg;
-          pidflag = 1;
-            
-         test1 = 1;
-        }
-        else
-        {
-          fprintf(stderr, "error: process ID list syntax error\n");
-          exit(EXIT_FAILURE);
-        }
-      }
-      else
-      {
-        // print error message and exit
-        fprintf(stderr, "error: list of process IDs must follow -p\n");
+    int opt = 0, i = 0;
+    flags *f = malloc(sizeof(flags));
+    if (f == NULL) {
+        fprintf(stderr, "unavailable to allocate memory for flags\n");
         exit(EXIT_FAILURE);
-      }
-
-      break;
-
-    case 'S':
-
-      if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
-      {
-        stimeflag = 0;
-      }
-      else if (optarg != NULL)
-      {
-          fprintf(stderr, "error: option syntax error\n");
-          exit(EXIT_FAILURE);
-      }
-      
-      else
-      {
-        stimeflag = 1;
-
-          test1=1;
-
-      }
-      break;
-
-    case 's':
-            
-      if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
-      {
-        stateflag = 0;
-      }
-      else if (optarg != NULL)
-      {
-          fprintf(stderr, "error: option syntax error\n");
-          exit(EXIT_FAILURE);
-      }
-      else
-      {
-        stateflag = 1;
-        test1=1;
-          
-      }
-      break;
-
-    case 'v':
-
-      if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
-      {
-        vmemflag = 0;
-      }
-      else if (optarg != NULL)
-      {
-          fprintf(stderr, "error: option syntax error\n");
-          exit(EXIT_FAILURE);
-      }
-      else
-      {
-        vmemflag = 1;
-          test1=1;
-      }
-      break;
-
-    case 'U':
-
-      if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
-      {
-        utimeflag = 0;
-      }
-      else if (optarg != NULL)
-      {
-          fprintf(stderr, "error: option syntax error\n");
-          exit(EXIT_FAILURE);
-      }
-      else
-      {
-        utimeflag = 1;
-          test1=1;
-      }
-      break;
-
-    case 'c':
-      if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
-      {
-        cmdflag = 0;
-      }
-      else if (optarg != NULL)
-      {
-          fprintf(stderr, "error: option syntax error\n");
-          exit(EXIT_FAILURE);
-      }
-      else
-      {
-        cmdflag = 1;
-          test1=1;
-      }
-
-      break;
-    case '?':
-      fprintf(stderr, "Error: unsupported option\n");
-      exit(EXIT_FAILURE);
-      break;
-    default:
-      fprintf(stderr, "Error: unsupported option\n");
-      break;
     }
-      
-  }
 
-    if (test1 == 0 && argc > 1){
+    /* Declare flags */
+    int pidflag = 0, stateflag = 0, stimeflag = 0, vmemflag = 0; // flags with default value False
+    int utimeflag = 1, cmdflag = 1;                              // flags with default value True
+    
+    
+    int flagtrue = 0; // Boolean if a flag  is changed to be true
+    
+    /* Read the user-command from the console */
+    while ((opt = getopt(argc, argv, "p:S::s::U::v::c::")) != -1) {
+        
+        // turn off error message
+        opterr = 0;
+        
+        /* Switch to options read from the command-line arguments */
+        switch (opt) {
+            
+            case 'p':
+                /* Check if the argument is valid */
+                if (optarg != NULL)
+                {
+                    int valid = 1, size = strlen(optarg);
+                    for (i = 0; i < size; i++)
+                    {
+                        if (!isdigit(optarg[i]))
+                        {
+                            valid = 0;
+                        }
+                    }
+                    /* Change pid flag true if the argument is true. Otherwise, exit */
+                    if (valid) 
+                    {
+                        f->pid = optarg;
+                        pidflag = 1;
+                        flagtrue = 1;
+                    }
+                    else
+                    {
+                        fprintf(stderr, "error: process ID list syntax error\n");
+                        exit(EXIT_FAILURE);
+                    }
+                }
+                /* Print error if no argument for p option is received*/
+                else {
+                    fprintf(stderr, "error: list of process IDs must follow -p\n");
+                    exit(EXIT_FAILURE);
+                }
+                break;
+
+            case 'S':
+                /* Check for no display option */
+                if ((optarg != NULL) && (strcmp(optarg, "-") == 0)) 
+                {
+                    stimeflag = 0;
+                }
+                /* Check for syntax error */
+                else if (optarg != NULL) 
+                {
+                    fprintf(stderr, "error: option syntax error\n");
+                    exit(EXIT_FAILURE);
+                }
+                /* Changes stime flag */
+                else 
+                {
+                    stimeflag = 1;
+                    flagtrue=1;
+                }
+                break;
+            
+            case 's':
+                /* Check for no display option */
+                if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
+                {
+                    stateflag = 0;
+                }
+                /* Check for syntax error */
+                else if (optarg != NULL){
+                    fprintf(stderr, "error: option syntax error\n");
+                    exit(EXIT_FAILURE);
+                }
+                /* Change state flag */
+                else{
+                    stateflag = 1;
+                    flagtrue = 1;
+                }
+                break;
+            
+            case 'v':
+                /* Check for no display option*/
+                if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
+                {
+                    vmemflag = 0;
+                } 
+                /* Check for syntax error*/
+                else if (optarg != NULL)
+                {
+                    fprintf(stderr, "error: option syntax error\n");
+                    exit(EXIT_FAILURE);
+                }
+                /* Change vmem flag*/
+                else
+                {
+                    vmemflag = 1;
+                    flagtrue=1;
+                }
+                break;
+
+            case 'U':
+                /* Check for no display option*/
+                if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
+                {
+                    utimeflag = 0;
+                }
+                /* Check for syntax error*/
+                else if (optarg != NULL)
+                {
+                    fprintf(stderr, "error: option syntax error\n");
+                    exit(EXIT_FAILURE);
+                }
+                /*Chanve utime flag*/
+                else
+                {
+                    utimeflag = 1;
+                    flagtrue = 1;
+                }
+                break;
+
+            case 'c':
+                /* Check for no display option */
+                if ((optarg != NULL) && (strcmp(optarg, "-") == 0))
+                {
+                    cmdflag = 0;
+                }
+                /* Check for syntax error */
+                else if (optarg != NULL)
+                {
+                    fprintf(stderr, "error: option syntax error\n");
+                    exit(EXIT_FAILURE);
+                }
+                /* Change cmd flag */
+                else
+                {
+                    cmdflag = 1;
+                    flagtrue = 1;
+                }
+                break;
+            
+            case '?':
+                /* Exit for any invalid option */
+                fprintf(stderr, "Error: unsupported option\n");
+                exit(EXIT_FAILURE);
+                break;
+            
+            default:
+                fprintf(stderr, "Error: unsupported option\n");
+                break;
+        }
+    }
+    /* Exit for cases when there is no option and there are strings of characters */
+    if (flagtrue == 0 && argc > 1)
+    {
         fprintf(stderr, "Error: unsupported option\n");
         exit(EXIT_FAILURE);    
     }
-
+    
+    /* Update flag */
     f->pidflag = pidflag;
     f->stateflag = stateflag;
     f->stimeflag = stimeflag;
@@ -199,6 +213,13 @@ flags *parseArgument(int argc, char *argv[])
     return f;
 }
 
+/*
+ * Function: findDigit
+ * --------------------
+ *
+ *  num : 
+ *  
+ */
 int findDigit(int num) {
     int digit = 0;
     do
@@ -210,37 +231,57 @@ int findDigit(int num) {
     return digit;
 }
 
-
-void readPidandPrint(flags *flag, char * pid) {
+/*
+ * Function: readPidandPrint
+ * --------------------
+ * This function receives flags and pid to get data related to the pid. Flags
+ * are used to display chosen features of pid.
+ *
+ *  flag : Flags struct that contains the flag of pid,state, user time, system
+ *         time, amount of virtual memory and command line.
+ *  pid : A pointer to an array of characters that stores pid value.
+ *  
+ */
+void readPidandPrint(flags *flag, char * pid) 
+{
+    
+    /* Arrays of characters that will be used as a path of a file to be open */
     const char * proc = "/proc/";
     const char * stat = "/stat";
     const char * statm ="/statm";
     const char * cmdline = "/cmdline";
     
+    /* Variables that stores the data related to the pid */
     char state;
     char * userTime;
     char * stime;
     char * vmem;
 
+    /* Array of characters that store path to stat file of pid */
     char * path = calloc((sizeof(proc) + sizeof(pid) + sizeof(stat)), sizeof(char));
     sprintf(path, "%s%s%s", proc, pid, stat);
 
     /**** OPENING STAT FILE ******/
     FILE* fp;
-    if ((fp = fopen(path, "r")) != NULL){
+    
+    /* Check if the path is valid */
+    if ((fp = fopen(path, "r")) != NULL)
+    {
         printf("%s: ", pid);
     }
     else {
         exit(EXIT_FAILURE);
     }
-    char line[255];
-    int count = 0;
     
+    char line[255]; // Array of charaters that read a line of file
+    int count = 0; // The number of spaces counted
     
-    
+    /* Read characters from the file */
     while (fscanf(fp, "%s", line) == 1)
     {
         count++;
+        
+        /* Read state if state flag is true */
         if (count == 3 && flag->stateflag)
         {
             state = line[0];
@@ -248,6 +289,7 @@ void readPidandPrint(flags *flag, char * pid) {
             memset(line, 0, 255);
         }
         
+        /* Read user time if utime flag is true */
         if (count == 14 && flag->utimeflag)
         {
             userTime = line;
@@ -255,11 +297,12 @@ void readPidandPrint(flags *flag, char * pid) {
             printf("utime=%s ", userTime);
             memset(line, 0, 255);
         }
-                   
+        
+        /* Read system time if stime flage is true */
         if (count == 15 && flag->stimeflag)
         {
             stime = line;
-            printf("%s ", stime);
+            printf("stime=%s ", stime);
             memset(line, 0, 255);
         }
     }
@@ -269,32 +312,40 @@ void readPidandPrint(flags *flag, char * pid) {
     /**** CLOSE FILE ****/
     fclose(fp);
     
+    /* Array of characters that store path to statm file of pid */
     path = calloc((sizeof(proc) + sizeof(pid) + sizeof(statm)), sizeof(char));
     sprintf(path, "%s%s%s", proc, pid, statm);
     
     /**** OPENING STATM FILE ****/
     fp = fopen(path, "r");
     
+    /* Read virtual memory used if vmemflag is true */
     if (fscanf(fp, "%s", line) == 1 && flag->vmemflag)
     {
         vmem = line;
-        printf("%s ", vmem); 
+        printf("vmem=%s ", vmem); 
         memset(line, 0, 255);
     }
 
     /**** CLOSE FILE ****/
     fclose(fp);
+    
     /**** FREE MEMORY ****/
     free(path);
     
-    /*** Opening CmdLine file ***/
+    /* Read command-line if cmdflag is true */
     if(flag->cmdflag){
+        /* Array of characters that store path to cmdline file of pid */
         path = calloc((sizeof(proc) + sizeof(pid) + sizeof(cmdline)), sizeof(char));
         sprintf(path, "%s%s%s", proc, pid, cmdline);
+        
         /**** OPENING CMDLINE FILE ****/
         fp = fopen(path, "r");
-        char word;
-        int i = 0;
+        
+        char word; // Character read from the file
+        int i = 0; // Index of a character array that will store the variable word
+        
+        /* Read non-null characters in the file*/
         while ((word = fgetc(fp)) != EOF){
             if (word !='\0'){
                 line[i] = word;
@@ -312,9 +363,15 @@ void readPidandPrint(flags *flag, char * pid) {
     }
 
     printf("\n");
-
 }
 
+/*
+ * Function: findDigit
+ * --------------------
+ *
+ *  num : 
+ *  
+ */
 char ** scanDirectory(flags *flag){
       
       // set path variables
