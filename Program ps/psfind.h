0b2cb9979ba41f81a7acd9cac855950dfa1d9008
Hyukjoon Yang <hyang348@wisc.edu>
2020-09-22 17:44:20 -0500

    Update psfind.h

--- Program ps/psfind.h
+++ Program ps/psfind.h
@@ -2,16 +2,12 @@
 * FILENAME :       psfind.h             
 *
 * DESCRIPTION :
-*       A simple 537 version of the Linux ps command
-*
-* PUBLIC FUNCTIONS :
-*       
+*       This is a header file for cs537 version of ps command program.
 *
 *
-*
-* AUTHOR :    Jiwon Song  HyukJun Yang   
-*
-*
+* AUTHOR :  Jiwon Song  HyukJoon Yang   
+* CLASS: CS 537
+* TERM: FALL 2020
 *
 **/
 
@@ -30,6 +26,8 @@
 
 #define MAX_PID 7
 #define MAX_NUM_PID 100
+#define BUFF_MAX 300
+#define LINE_MAX 255
 
 typedef struct Flags{
     int pidflag;
@@ -42,11 +40,54 @@ typedef struct Flags{
 
 } flags;
 
+/*
+ * Function:  parseArgument 
+ * --------------------
+ * parses command line argument and set up flags
+ *    
+ *  argc: number of argument passed
+ *  argv: a pointer array which points to each argument passed to the program
+ *
+ *  returns: flags object with each flag set up according to command line argument
+ */
 flags * parseArgument(int argc, char * argv[]);
 
-char ** scanDirectory(flags *flag);
+/*
+ * Function:  scanDirectory
+ * --------------------
+ * reads all the files in proc/ directory, 
+ *      get process id list that are currently running under user process id
+ *
+ *
+ *  returns: A char double array filled with process id
+ *
+ */
+
+char ** scanDirectory();
+
+/*
+ * Function:  readPidandPrint 
+ * --------------------
+ * opens a process file under proc/ directory,
+ *      extracts information according to the passed flag
+ *      and prints out result
+ *
+ *  flag: a flag pointer to access each flag
+ *  pid: a char pointer pointing to process id
+ *
+ */
 
 void readPidandPrint(flags *flag, char * pid);
+
+/*
+ * Function:  findDigit
+ * --------------------
+ * simply computes a number of digits of passed integer
+ *
+ *  num: an integer to find digits
+ *
+ *  returns: a number of digits
+ */
 
 int findDigit(int num);
 
